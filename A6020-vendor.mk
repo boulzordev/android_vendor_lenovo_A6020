e7a392a7a5a3ad66c57354b8e4de2bd5cb8518b2
Daniel Stuart <daniel.stuart14@gmail.com>
2017-02-26 21:17:58 -0300

    k5: remove rfs_access blob
    That's for debug only.
    
    Change-Id: Icce50af718e55abf88cf4d01f683a069824aef37

--- A6020-vendor.mk
+++ A6020-vendor.mk
@@ -30,7 +30,6 @@ PRODUCT_COPY_FILES += \
     vendor/lenovo/A6020/proprietary/bin/qmuxd:system/bin/qmuxd \
     vendor/lenovo/A6020/proprietary/bin/qseecomd:system/bin/qseecomd \
     vendor/lenovo/A6020/proprietary/bin/radish:system/bin/radish \
-    vendor/lenovo/A6020/proprietary/bin/rfs_access:system/bin/rfs_access \
     vendor/lenovo/A6020/proprietary/bin/rild:system/bin/rild \
     vendor/lenovo/A6020/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/lenovo/A6020/proprietary/bin/thermal-engine:system/bin/thermal-engine \
