fb672ba9838e1da00b58342b29603d692e14d0ad
Bruno Martins <bgcngm@gmail.com>
2017-12-11 20:48:30 +0100

    A6020: include newest FM HAL dependency blobs
    Change-Id: I1006fb96928b3ec11a0bb8e5b9263a59ff080655

--- A6020/Android.mk
+++ A6020/Android.mk
@@ -18,6 +18,17 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter A6020,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := vendor.qti.hardware.fm@1.0
+LOCAL_MODULE_OWNER := lenovo
+LOCAL_SRC_FILES_32 := proprietary/lib/vendor.qti.hardware.fm@1.0.so
+LOCAL_SRC_FILES_64 := proprietary/lib64/vendor.qti.hardware.fm@1.0.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := TimeService
 LOCAL_MODULE_OWNER := lenovo
 LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk
